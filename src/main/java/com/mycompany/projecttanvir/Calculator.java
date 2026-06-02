3ffe5d982797ba0efa001c0ea34c9f0ef9f11609
tanvi <tanvi@10.0.0.8>
2026-06-01 21:36:29 -0400

    Added 4 functions of Add, subtract, divide and multiply in the Calculator class

--- src/main/java/com/mycompany/projecttanvir/Calculator.java
+++ src/main/java/com/mycompany/projecttanvir/Calculator.java
@@ -9,5 +9,30 @@ package com.mycompany.projecttanvir;
  * @author tanvi
  */
 public class Calculator {
-    
+
+    // Addition
+    public double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    // Subtraction
+    public double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    // Multiplication
+    public double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    // Division
+    public double divide(double num1, double num2) {
+
+        if (num2 == 0) {
+            System.out.println("Cannot divide by zero.");
+            return 0;
+        }
+
+        return num1 / num2;
+    }
 }
