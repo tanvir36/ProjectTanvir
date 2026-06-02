1b12adfd56cb73eb4303aee7d126e3d0befa4858
tanvi <tanvi@10.0.0.8>
2026-06-01 21:39:55 -0400

    Added the functionality into the main class

--- src/main/java/com/mycompany/projecttanvir/ProjectTanvir.java
+++ src/main/java/com/mycompany/projecttanvir/ProjectTanvir.java
@@ -8,9 +8,58 @@ package com.mycompany.projecttanvir;
  *
  * @author tanvi
  */
+import java.util.Scanner;
 public class ProjectTanvir {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner input = new Scanner(System.in);
+        Calculator calc = new Calculator();
+
+        System.out.println("=== Simple Calculator ===");
+
+        System.out.print("Enter first number: ");
+        double num1 = input.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = input.nextDouble();
+
+        System.out.println("\nChoose Operation:");
+        System.out.println("1. Add");
+        System.out.println("2. Subtract");
+        System.out.println("3. Multiply");
+        System.out.println("4. Divide");
+
+        System.out.print("Enter choice: ");
+        int choice = input.nextInt();
+
+        double result = 0;
+
+        switch (choice) {
+
+            case 1:
+                result = calc.add(num1, num2);
+                System.out.println("Result = " + result);
+                break;
+
+            case 2:
+                result = calc.subtract(num1, num2);
+                System.out.println("Result = " + result);
+                break;
+
+            case 3:
+                result = calc.multiply(num1, num2);
+                System.out.println("Result = " + result);
+                break;
+
+            case 4:
+                result = calc.divide(num1, num2);
+                System.out.println("Result = " + result);
+                break;
+
+            default:
+                System.out.println("Invalid choice.");
+        }
+
+        input.close();
     }
 }
